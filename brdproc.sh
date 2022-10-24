6b48368a7cf6c7d53ac07e6072fec5e499507f47
Aedan Cullen <aedan@aedancullen.com>
2022-10-24 05:42:52 -0500

    Update layers in brdproc.sh

--- brdproc.sh
+++ brdproc.sh
@@ -1,4 +1,4 @@
 sed -i 's/layer="21"/layer="121"/g' $1
 sed -i 's/layer="22"/layer="122"/g' $1
-sed -i 's/layer="136"/layer="21"/g' $1
-sed -i 's/layer="137"/layer="22"/g' $1
+sed -i 's/layer="131"/layer="21"/g' $1
+sed -i 's/layer="132"/layer="22"/g' $1
